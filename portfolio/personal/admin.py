04596fa8ebb63c883a251751ada6d8a1f55516f9
Cris Ruiz <crisruizrn@gmail.com>
2023-08-01 13:56:52 -0600

    adding the final logic of the models and creation of objects

--- portfolio/personal/admin.py
+++ portfolio/personal/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from personal.models import Skill, Education, Work, SuccessStory
 
 # Register your models here.
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkAdmin(admin.ModelAdmin):
+    pass
+
+
+class SuccessAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Work, WorkAdmin)
+admin.site.register(SuccessStory, SuccessAdmin)
--- portfolio/personal/models.py
+++ portfolio/personal/models.py
@@ -13,6 +13,9 @@ class Skill(models.Model):
     skill_name = models.CharField(max_length=100, unique=True)
     percentaga = models.PositiveIntegerField(default=10, validators=[one_to_houndred_validation])
 
+    def __str__(self):
+        return self.skill_name
+
 
 class Education(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -21,12 +24,15 @@ class Education(models.Model):
     university = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class Work(models.Model):
     title = models.CharField(max_length=200)
     company = models.CharField(max_length=150)
-    start_date = models.DateField(auto_now=True)
-    end_date = models.DateField(auto_now=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True)
 
     def __str__(self):
         return f"{self.title} {self.company}"
